86c9898bf26950761270f59109092b808a667329
rawaldeep <rawaldeepsingh26@gmail.com>
2020-07-15 17:30:48 +0200

    design complete with issue in chrome

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-let el = document.querySelectorAll('#PrizmWrapper .ps-timeline-sec .container ol.ps-timeline li span');
-let ele = document.querySelectorAll('#PrizmWrapper .ps-timeline-sec .container ol.ps-timeline li');
-function hasClass(element, className) {
-    return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
-}
-ele.forEach(function(item, index){
-item.addEventListener('click', function(){
-    for(let i =0; i< ele.length; i++){
-        if(hasClass(ele[i].querySelector('span'), 'active')){
-            ele[i].querySelector('span').classList.remove('active');
+let line = document.querySelector(".timelinearea");
+let points = line.querySelectorAll("ul li .dot");
+let par = document.querySelector('.slideshow-container');
+let el = par.querySelectorAll('.mySlides');
+let blocks = line.querySelectorAll('ul li')
+
+points.forEach( point =>{
+    point.addEventListener("click", ()=>{
+        let position = point.parentNode.dataset.position - 1;
+        points.forEach(oldpoints=>{
+            if(oldpoints.classList.contains("activedot")){
+                oldpoints.classList.remove("activedot")
+            }
+        });
+        blocks.forEach(block=>{
+            if(block.classList.contains("activeblock")){
+                block.classList.remove("activeblock")
+            }
+        });
+        point.classList.add("activedot");
+        blocks[position].classList.add("activeblock");
+        let slider = document.querySelector('.slideshow-container');
+        slider.style.left = '-'+33*position+'em';
+        el.forEach(slide=>{
+            if(slide.classList.contains("slideactive")){
+                slide.classList.remove("slideactive")
+            }
+            if(slide.classList.contains("commingslide")){
+                slide.classList.remove("commingslide")
+            }
+        })
+        let newpos = position -1;
+        for(let i= newpos; i > 0;i--){
+            el[i].classList.add("commingslide");
         }
-    }
-    item.querySelector('span').classList.add('active');
-    
-    if(item.getAttribute('data-position')){
-      currentSlide(item.getAttribute('data-position'));
-    }
-
+        el[position].classList.add("slideactive");
+        el[position+1].classList.add("commingslide");
+    });
 })
-})
-let slideIndex = 5;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  let index = parseInt(n)
-  showSlides(slideIndex = index);
-document.getElementsByClassName("mySlides")[n-1].scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
-}
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-      slides[i].style.position = 'absolute';
-      slides[slideIndex-1].style.right = '-100%';
-
-    }
-  slides[slideIndex-1].style.display = "block";
-  slides[slideIndex-1].style.position = 'static';
-  slides[slideIndex-1].style.right = 'inherit';
-}
